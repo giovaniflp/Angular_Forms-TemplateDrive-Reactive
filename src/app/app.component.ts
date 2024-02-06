b2d2eba891e33a9b213a861e8f2f319935bdf656
giovaniflp <giovanifeitosa123@gmail.com>
2024-02-06 02:43:22 -0300

    estudo sobre template driven, input text e validações!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TemplateDrivenComponent } from './components/forms/template-driven/template-driven.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TemplateDrivenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/forms/template-driven/template-driven.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-driven',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './template-driven.component.html',
+  styleUrl: './template-driven.component.css'
+})
+export class TemplateDrivenComponent {
+
+}
