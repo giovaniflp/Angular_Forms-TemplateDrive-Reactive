96a8e4f49d4cee696163d77f12fafdb851fe4e14
giovaniflp <giovanifeitosa123@gmail.com>
2024-02-08 02:53:16 -0300

    Finalização de estudos sobre Forms e manipulação de Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TemplateDrivenComponent } from './components/forms/template-driven/template-driven.component';
+import { ReactiveFormsComponent } from './components/forms/reactive-forms/reactive-forms.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TemplateDrivenComponent],
+  imports: [CommonModule, RouterOutlet, TemplateDrivenComponent, ReactiveFormsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/forms/reactive-forms/reactive-forms.component.ts
+++ src/app/components/forms/reactive-forms/reactive-forms.component.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-forms.component.html',
   styleUrl: './reactive-forms.component.css'
 })
 export class ReactiveFormsComponent {
 
+  constructor(
+    private _fB: FormBuilder
+  ){
+
+  }
+
+  public profileForm = this._fB.group({
+    name: ['', [Validators.minLength(2), Validators.maxLength(5)]],
+    mystacks: this._fB.group({
+      front: ['Angular 17'],
+      back: ['Spring Boot 3']
+    }),
+    myFavoriteFoods: this._fB.array([
+      ["Pizza"],
+      ["Burguer"],
+    ])
+  });
+
+  public update(){
+    this.profileForm.patchValue({
+      name: "Clodivaldo",
+      mystacks: {
+        front: "HTML 4",
+        back: "PHP 5"
+      }
+    })
+  }
+
+  public addFavoriteFood(newfood: string){
+    const newFood = this.profileForm.get('myFavoriteFoods') as FormArray;
+    const addItens = new FormControl(newfood)
+
+    newFood.push(addItens);
+  }
+
+  public submit (){
+    if(this.profileForm.valid){
+      console.log("Formulário válido")
+      console.log(this.profileForm.value)
+      console.log(this.profileForm.valid)
+  } else{
+    console.log("Formulário inválido")
+    console.log(this.profileForm.value)
+  }
+}
 }
